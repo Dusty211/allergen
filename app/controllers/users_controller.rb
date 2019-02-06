45fd3e6331e97eb480fa8965d638a25aeb51faf4
smaraghi <maraghiserven@gmail.com>
2019-02-06 16:48:27 -0500

    controllers done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.destroy(params[:id])
+    redirect_to users_path
+  end
 end
